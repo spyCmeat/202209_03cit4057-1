5715e09f57910d9aef2ad884035949cde120198e
spyCmeat <samleung180702@gmail.com>
2022-10-27 12:18:52 +0800

    Update lab_634AD398_1.py
    mistake in line 10 corrected

--- lab02/src/22559861/lab_634AD398_1.py
+++ lab02/src/22559861/lab_634AD398_1.py
@@ -7,7 +7,7 @@ def answer(maxNumber):
         for j in range(0,i+1):
             print("*",end=" ")
         print("\r")
-    for i in range (maxNumber+1,0,-1):
+    for i in range (maxNumber,0,-1):
         for j in range(0, i-1):
             print("*",end=" ")
         print("\r")
